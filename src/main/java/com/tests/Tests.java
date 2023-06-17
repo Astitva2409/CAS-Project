fc601763285d8c1e6e43b64fd86c2b2c1540be02
Astitva Singh <singhastitva99@gmail.com>
2023-06-17 20:41:21 +0530

    Hello

--- src/main/java/com/tests/Tests.java
+++ src/main/java/com/tests/Tests.java
@@ -30,7 +30,7 @@ public class Tests extends BaseClass {
         displayOrderAmount("//div[@class='_30jeq3 _16Jk6d']");
         elementClick("//ul[@class='row']/li/button[text()='Add to cart']");
         elementClick("//ul[@class='row']/li/button[text()='Add to cart']");
-        elementClick("//a[@class='_3SkBxJ']//span");
+        elementClick("//div[@class='_1psGvi']//span");
         takeScreenShot();
         verifyCart("//div[@class='zab8Yh _10k93p']");
         printPrice("//div[@class='_1dqRvU']//span", 2);
